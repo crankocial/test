caeb0363534326b89fc443034c82d11e7d6dd86d
crank_ocial <kirill201098@mail.ru>
2018-09-27 11:37:47 +0300

    PRINT A>B

--- DGV_1.py
+++ DGV_1.py
@@ -2,11 +2,24 @@ def foo (x, y):
     c = x > y
     return c
 
-print(foo(1,2))
-
 def fun(a,b):
   max = a if foo(a,b) else b
   return max
 
-print (fun(1,2))
+
+
+print ("enter numbers to compare")
+print ("a=", end='')
+a=input()
+print ("b=", end='')
+b=input()
+
+if fun(a,b)==a:
+    min=b;
+else:
+
+    min=a;
+
+
+print (fun(a,b), '>', min)
 
